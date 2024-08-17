fa7bbe0723c32a93e330e4cdd2736badfb8576d4
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2024-08-16 21:31:16 -0500

    Clase04

--- Programacion Web/webempleados/core/admin.py
+++ Programacion Web/webempleados/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Empleados
+from .models import Empleados,Pasaporte,Direcciones,Curso
 
-admin.site.register(Empleados)
+admin.site.register([Empleados,Pasaporte,Direcciones,Curso])
 
 
--- Programacion Web/webempleados/core/models.py
+++ Programacion Web/webempleados/core/models.py
@@ -5,7 +5,27 @@ class Empleados(models.Model):
     apellido=models.CharField(max_length=50)
     email=models.EmailField(max_length=50)
     salario=models.DecimalField(max_digits=10,decimal_places=2)
+    cargo=models.CharField(max_length=50,blank=True,null=True)
 
     def __str__(self):
         return self.nombre
 
+class Pasaporte(models.Model):
+    empleado=models.OneToOneField(Empleados,on_delete=models.CASCADE)
+    pasaporte_numero=models.CharField(max_length=50)
+
+
+class Direcciones(models.Model):
+    empleado=models.ForeignKey(Empleados,on_delete=models.CASCADE)
+    direccion=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.direccion
+    
+ #En este ejemplo un empleado puede tener muchas direcciones pero cada direccion solo pertenece a un empleado   
+
+class Curso(models.Model):
+    nombre=models.CharField(max_length=200)
+    empleados=models.ManyToManyField(Empleados)
+
+    
